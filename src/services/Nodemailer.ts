39eed3c1b2b7b2be54095e493050030706f6c528
eduardommelo <fobbie.gov@gmail.com>
2023-08-29 21:04:04 -0300

    feat: adicionado service Nodemailer

---
+++ src/services/Nodemailer.ts
@@ -0,0 +1,34 @@
+import { Transporter } from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport/index.js";
+import { MailerType } from "./types/Mailer.js";
+import { Render } from "./presentation/Render.js";
+import Mail from "nodemailer/lib/mailer/index.js";
+
+export class Nodemailer extends Render {
+  private html: string | unknown = "";
+  constructor(
+    private readonly transporter: Transporter<SMTPTransport.SentMessageInfo>,
+    private readonly client?: MailerType.Options
+  ) {
+    super(client?.templates);
+  }
+
+  async renderReact(templateName: string) {
+    const html = await super.react(templateName);
+    this.html = html;
+    return this;
+  }
+
+  async renderEngineView(templateName: string, data?: any) {
+    const html = await super.engineView(templateName, data);
+    this.html = html;
+
+    return this;
+  }
+
+  async send(mailOptions: Mail.Options) {
+    if (this.html) mailOptions.html = this.html;
+
+    return await this.transporter.sendMail(mailOptions);
+  }
+}
